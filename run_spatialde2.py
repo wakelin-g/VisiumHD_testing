07c7f4de6140d34e441d0560fa1bb412909704e1
Griffen Wakelin <wakelin@dal.ca>
2024-12-20 21:28:02 -0400

    updated SpatialDE->SpatialDE2 name in run_spatialde2 script

--- run_spatialde2.py
+++ run_spatialde2.py
@@ -8,5 +8,5 @@ out_dir = "outputs/"
 sdata = sd.read_zarr("data.zarr")
 adata = sdata["square_016um"].copy()
 
-spatialde2_test = SpatialDE.test(adata, stack_kernels=False, use_cache=False)
+spatialde2_test = SpatialDE2.test(adata, stack_kernels=False, use_cache=False)
 spatialde2_test.to_csv(os.path.join(out_dir, "spatialde2_outputs.csv"))
